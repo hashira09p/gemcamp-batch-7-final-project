cb5388ad52d18025d7d8b0ede1c559536477ade0
Je Rome <jeromeabelida19@gmail.com>
2024-10-25 18:17:12 +0800

    added: routes for admin and client

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
+  authenticated :user, ->(user) { user.admin? } do
+    root 'admin/home#index', as: :admin_root
+  end
+
+  authenticated :user, ->(user) { user.client? } do
+    root 'client/home#index', as: :client_root
+  end
+
+  unauthenticated do
+    root 'home#index'
+  end
 end
+
